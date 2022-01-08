bd419480ea3954e99f8df6ad324a8b89fca3b85b
rkp678 <rkpierce678@gmail.com>
2022-01-08 10:58:24 -0600

    Pushing updated spyder and JL code

--- starter_code.py
+++ starter_code.py
@@ -1,25 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jan  8 09:33:08 2022
-
-@author: Margee
-"""
-
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jan  6 21:43:04 2022
-
-@author: morte
-"""
-
 import json
 import pandas as pd
+# %%
 
-# =============================================================================
-# import data
-# =============================================================================
-
-filepath = "C:/jobs_india/"
+# Create File Path
+filepath = "C:/Users/piercerachel/Desktop/git/Project2_GroupC/"
 filename = "monster_india_latest_jobs_free_dataset.json"
 
 # Opening JSON file
@@ -27,31 +11,20 @@ f = open(filepath + filename)
 
 # returns JSON object as a dictionary
 data = json.load(f)
-
 # %%
 
-listy = [1, 2, 3, 4, 5]
-
-for each in listy:
-    print(each)
-
-# %%
-
+# Create data list
 address_country = []
 address_locality = []
 address_region = []
 company = []
 description = []
-expires_at = []
 industry = []
 postal_code = []
 posted_at = []
-qualification = []
 salary = []  # the target
 salary_type = []
-scraped_at = []
-skill = []
-street_address = []
+skills = []
 title = []
 url = []
 
@@ -64,9 +37,14 @@ for each in data:
     address_region.append(each["address_region"])
     company.append(each["company"])
     description.append(each["description"])
-    expires_at.append(each["expires_at"])
-    salary.append(each["salary"])  # the target
-
+    industry.append(each["industry"])
+    postal_code.append(each["postal_code"])
+    posted_at.append(each["posted_at"])
+    salary.append(each["salary"])
+    salary_type.append(each["salary_type"])
+    skills.append(each["skills"]) 
+    title.append(each["title"])
+    url.append(each["url"])
 # %%
 
 # create df, using string header and list as rows
@@ -75,11 +53,37 @@ df = pd.DataFrame({"address_country": address_country,
                    "address_region": address_region,
                    "company": company,
                    "description": description,
-                   "expires_at": expires_at,
-                   "salary": salary})  # the target
+                   "industry": industry,
+                   "postal_code": postal_code,
+                   "posted_at": posted_at,
+                   "salary": salary,
+                   "salary_type": salary_type,
+                   "skills": skills,
+                   "title": title,
+                   "url": url,
+                  })
 
 # %%
 
-# =============================================================================
-# clean data
-# =============================================================================
+# Preview dataframe
+df.head()
+
+# %%
+
+# Clean data, drop unnecessary columns
+df.drop(['address_country', 'address_locality', 'address_region', 'postal_code', 'salary_type', 'url'], axis=1, inplace=True)
+
+# %%
+
+df.head()
+
+# %%
+
+# Drop any salaries that are Not disclosed
+df = df[df["salary"]!="Not disclosed"]
+df.head()
+
+# %%
+
+# Check if there are any NAs/nulls
+df.isnull().sum()
